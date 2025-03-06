11ace905c793ef05451d2ae5d0a99adb1ba8cc78
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-03-06 16:42:19 +0530

    Update powerhouse.py

--- powerhouse.py
+++ powerhouse.py
@@ -109,5 +109,5 @@ def init_webhook():
 ###############################################################################
 # Run the app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get('PORT', 8000))  # Use Azure's port
+    app.run(host='0.0.0.0', port=port, debug=True)
